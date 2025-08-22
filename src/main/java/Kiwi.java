e00d89e8dfe691d206d6a54edf7de30f1d41f5d0
Zayan <zayankasim1@gmail.com>
2025-08-22 18:31:42 +0800

    Basic loop strucutre and exit feature

--- src/main/java/Kiwi.java
+++ src/main/java/Kiwi.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Kiwi {
     public static void main(String[] args) {
+        System.out.println("___________________________________");
         String logo = " _  __  _  _      _  _ \n"
                 + "| |/ / | || |    | || |\n"
                 + "| ' /  | || | __ | || |\n"
@@ -9,7 +12,23 @@ public class Kiwi {
         System.out.println("___________________________________");
         System.out.println("What can I do for you? \n");
         System.out.println("___________________________________");
-        System.out.println("Bye, see you again :)");
 
+        Scanner input = new Scanner(System.in);
+
+        boolean end = false;
+        while (!end) {
+            System.out.println("Your query: ");
+            String line = input.nextLine();
+            System.out.println("___________________________________");
+            if (line.equalsIgnoreCase("bye")) {
+                end = true;
+                System.out.println("Bye, see you again :)");
+                System.out.println("___________________________________");
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println("___________________________________");
+            }
+        }
     }
 }

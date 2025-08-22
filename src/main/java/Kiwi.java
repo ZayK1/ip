6d8cf55368449b4df4a24630a0cd4f1084f80c8f
Zayan <zayankasim1@gmail.com>
2025-08-22 17:56:49 +0800

    Rename Chatbot

---
+++ src/main/java/Kiwi.java
@@ -0,0 +1,15 @@
+public class Kiwi {
+    public static void main(String[] args) {
+        String logo = " _  __  _  _      _  _ \n"
+                + "| |/ / | || |    | || |\n"
+                + "| ' /  | || | __ | || |\n"
+                + "| . \\  | || |/ _\\| || |\n"
+                + "|_|\\_\\ |_|||_\\__/|_||_|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("___________________________________");
+        System.out.println("What can I do for you? \n");
+        System.out.println("___________________________________");
+        System.out.println("Bye, see you again :)");
+
+    }
+}

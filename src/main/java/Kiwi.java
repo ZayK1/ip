5396d7ea7970d75fc85fc9bf3f110f2608d61a0d
Zayan <zayankasim1@gmail.com>
2025-08-24 02:03:43 +0800

    Level 2 compelete: Refactor of previous levels to adhere to OOP principles

--- src/main/java/Kiwi.java
+++ src/main/java/Kiwi.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
+
 public class Kiwi {
-    public static void main(String[] args) {
+
+    private final ArrayList<String> todoList = new ArrayList<>();
+    private final Scanner input = new Scanner(System.in);
+
+    public void run() {
         System.out.println("___________________________________");
         String logo = " _  __  _  _      _  _ \n"
                 + "| |/ / | || |    | || |\n"
@@ -13,22 +20,45 @@ public class Kiwi {
         System.out.println("What can I do for you? \n");
         System.out.println("___________________________________");
 
-        Scanner input = new Scanner(System.in);
 
-        boolean end = false;
-        while (!end) {
+        while (true) {
             System.out.println("Your query: ");
             String line = input.nextLine();
             System.out.println("___________________________________");
             if (line.equalsIgnoreCase("bye")) {
-                end = true;
                 System.out.println("Bye, see you again :)");
                 System.out.println("___________________________________");
                 break;
+            } else if (line.equalsIgnoreCase("List")) {
+                System.out.println(displayTodoList(todoList));
+                System.out.println("___________________________________");
             } else {
-                System.out.println(line);
+                addTask(line);
                 System.out.println("___________________________________");
             }
         }
+    }
+
+    public void addTask(String task) {
+        todoList.add(task);
+        System.out.println("added: " + task);
+
+    }
+
+    public String displayTodoList(ArrayList<String> tasks) {
+        if (tasks.isEmpty()) { return "No tasks left for today :)";}
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (String task : tasks) {
+            count += 1;
+            sb.append(count + ". " + task + "\n");
+        }
+        sb.append("Tasks left to complete: " + count);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Kiwi kiwi = new Kiwi();
+        kiwi.run();
     }
 }
